fb4c02f23b601ae1ff04580254371bbdc023efea
Nisha-Bhattarai <c0840054@mylambton.ca>
2023-01-04 17:31:35 -0500

    Added verification of JWT Token and added GET HTTP req for user

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -49,7 +49,7 @@ const login = async (req, res, next) => {
   if (!isPasswordCorrect) {
     return res.status(400).json({ message: "Invalid Email/Password" });
   }
-  const token = jwt.sign({ id: existingUser.id }, JWT_SECRET_KEY, {
+  const token = jwt.sign({ id: existingUser._id }, JWT_SECRET_KEY, {
     expiresIn: "1hr",
   });
   return res
@@ -58,9 +58,35 @@ const login = async (req, res, next) => {
 };
 
 const verifyToken = (req, res, next) => {
-  const headers = req.headers(`authorization`);
-  console.log(headers);
+  const headers = req.headers["authorization"];
+  const token = headers.split(" ")[1];
+  if (!token) {
+    res.status(404).json({ message: "No token found" });
+  }
+  jwt.verify(String(token), JWT_SECRET_KEY, (err, user) => {
+    if (err) {
+      res.status(400).json({ message: "Invalid Token" });
+    }
+    console.log(user.id);
+    req.id = user.id;
+  });
+  next();
+};
+
+const getUser = async (req, res, next) => {
+  const userId = req.id;
+  let user;
+  try {
+    user = await User.findById(userId, "-password");
+  } catch (err) {
+    return new Error(err);
+  }
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
+  return res.status(200).json({ user });
 };
 exports.signup = signup;
 exports.login = login;
 exports.verifyToken = verifyToken;
+exports.getUser = getUser;
